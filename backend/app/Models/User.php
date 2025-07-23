477ef29af2493f8b369276d2c80448e7393e49e2
Nathan <iurynathan@gmail.com>
2025-07-23 17:26:47 -0300

    feat(models): estende User com integração PIX
    - Relacionamento hasMany com PixPayments
    - Método getPixStatistics() para analytics
    - Integração HasApiTokens do Sanctum para autenticação API

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,16 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
